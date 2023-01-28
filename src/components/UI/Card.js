d8a4c70f85a02854431548bcf2238dec7038a310
Lugard Agu <agulugard@gmail.com>
2023-01-28 15:46:38 +0100

    update  books UI pages files

---
+++ src/components/UI/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+
+import classes from './Card.module.css';
+
+const Card = (props) => {
+  const { extraclass, children } = props;
+
+  return (
+    <div
+      className={extraclass ? `${classes.card} ${extraclass}` : classes.card}
+    >
+      {children}
+    </div>
+  );
+};
+
+Card.propTypes = {
+  extraclass: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Card.defaultProps = {
+  extraclass: '',
+};
+
+export default Card;
