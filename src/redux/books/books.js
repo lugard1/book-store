7d1817e810c6b7c9e89cd338b51cba07ab6c9e5e
Lugard Agu <agulugard@gmail.com>
2023-01-31 17:47:55 +0100

    fix linters error

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,11 @@
-import { createReducer, createAction} from '@reduxjs/toolkit';
+import { createReducer, createAction } from '@reduxjs/toolkit';
 
 export const ADD_BOOK = createAction('books/add');
 export const REMOVE_BOOK = createAction('book/remove');
 
 const initialState = {
   books: [],
-}
+};
 
 const Reducer = createReducer(initialState, (develop) => {
   develop.addCase(ADD_BOOK, (state, action) => {
